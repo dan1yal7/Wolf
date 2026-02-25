f493808d989f09ce34a74fd3b32d2ed9b97cc32f
Daniyal <daniiltemirkhanov@gmail.com>
2026-02-25 23:59:02 +0500

    feat: initial commit

---
+++ WolfClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WolfClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WolfClient/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WolfClient")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WolfClient.Pages")]
