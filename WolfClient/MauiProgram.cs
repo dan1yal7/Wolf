1b00102dd90529e3c97b13bed5094f44f819b785
Daniyal Temirkhanov <141769930+dan1yal7@users.noreply.github.com>
2026-04-26 06:52:28 -0700

    feat: sub header with branch dropdown-btn

--- WolfClient/MauiProgram.cs
+++ WolfClient/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace WolfClient
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fa-solid-900.ttf", "FontAwesome");
                 });
 
 #if DEBUG
@@ -21,5 +22,5 @@ namespace WolfClient
 
             return builder.Build();
         }
-    }
+    }   
 }
