3686bd450abcaa0e4ef3148d4f97afcdd195f120
Daniyal Temirkhanov <141769930+dan1yal7@users.noreply.github.com>
2026-08-12 11:55:18 -0700

    feat: add recent repository model for list of recent repos for quick launch

---
+++ WolfClient/Models/RecentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfClient.Models
+{
+    public class RecentRepository
+    {
+        public string? Path { get; set; }
+        public string? Name { get; set; }
+        public DateTime LastOpenedUtc { get; set; }
+    }
+}
